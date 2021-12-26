7f2d15e0881b68aedb6060b4c72ff27283cc2690
youqinSUN <xiaoouvivian.it@gmail.com>
2021-12-26 02:11:53 +0200

    part3 3.13-3.14

--- index.js
+++ index.js
@@ -1,42 +1,14 @@
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
 
-const PORT = process.env.PORT || 3001
-
-
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const Contact = require('./models/contact')
+
 const app = express()
 
 
 app.use(express.json())//express json-parser
 app.use(express.static('build'))
-app.use(morgan('tiny'))
-
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
 
 morgan.token('type', (req) => {
     return req.method === 'POST'
@@ -59,53 +31,56 @@ app.use(morgan(function (tokens, req, res) {
 //routes
 app.get('/info', (req, res) => {
     const currentTime = new Date()
-    const info = `<p>Phonebook has info for ${persons.length} people.</p> 
-                  <p>${currentTime}</p>`
-    res.send(info)
+    Contact.find({}).then(contact => {
+        const info = `<p>Phonebook has info for ${contact.length} people.</p> 
+                      <p>${currentTime}</p>`
+        res.send(info)
+    })
 })
 
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Contact.find({}).then(contact => {
+        res.json(contact)
+    })
 })
 
 app.get('/api/persons/:id', (req, res) => {
     console.log(req.params)
     const id = Number(req.params.id)
-    const person = persons.find(p => p.id === id)
-    if (person) {
-        res.json(person)
-    } else {
-        res.status(404).end()
-    }
+    Contact.findById(req.params.id).then(contact => {
+        res.json(contact)
+    })
 })
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
     console.log(request.body)
-    const alreadyExist = persons.some(p => p.name === body.name)
+    //const alreadyExist = persons.some(p => p.name === body.name)
     if (!body || !body.name || !body.number) {
         return response.status(400).json({
             error: 'content missing'
         })
-    } else if (alreadyExist) {
+    } /*else if (alreadyExist) {
         return response.status(409).json({
             error: 'person with same name already exists'
         })
-    }
+    }*/
 
-    const generateId = () => {
-        const maxId = persons.length === 0 ? 0 : Math.max(...persons.map(p => p.id))
-        return maxId + 1
-    }
-
-    const person = {
-        id: generateId(),
+    const contact = new Contact({
         name: body.name,
         number: body.number
-    }
+    })
 
-    persons = persons.concat(person)
-    console.log(persons)
-    response.json(person)
+    contact.save().then(newContact => {
+        response.json(newContact)
+    })
+})
+
+
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
 })
